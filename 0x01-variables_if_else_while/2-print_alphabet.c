0255fdc454f296ce8f3853565cf7f8594edfcb6a
Fredrick <otienofredricko409@gmail.com>
2023-07-14 06:43:23 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: Always(Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putcher(c);
+	putcher('\n');
+	return (0);
+}
