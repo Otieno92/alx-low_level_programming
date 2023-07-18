166e8b9157f777e6eaceda60cccf7c6f3a1a5d6d
Fredrick <otienofredricko409@gmail.com>
2023-07-18 05:50:55 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include "main.h"
+
 /**
- * main - check the code.
+ * print_alphabet_x10 - Make alphabet x10 times
  *
- * Return: Always 0.
+ * Return: void
  */
-int main(void)
-	void print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
 	char c;
 	int i = 0;
