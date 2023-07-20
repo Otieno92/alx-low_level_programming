875de1e0c2e60c1d7352302ad7e8b8a5c76f8391
Fredrick <otienofredricko409@gmail.com>
2023-07-20 08:23:47 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void print_square(int size)
+{
+	int i;
+
+	if (size < 0)
+	{
+		_putchar('\n');
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		_putchar('#');
+	}
+	_putchar('\n');
+}
