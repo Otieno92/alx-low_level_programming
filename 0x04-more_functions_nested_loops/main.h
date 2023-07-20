5f8d3d75ebefc26ca5b4b21092c856789ac4bf12
Fredrick <otienofredricko409@gmail.com>
2023-07-20 09:25:53 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,5 +3,7 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
+void print_diagonal(int n);
 void print_square(int size);
 void _putchar(char c);
+void print_triangle(int size);
