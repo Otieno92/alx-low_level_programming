bca649bb71178187a6d78464e983377c9ca7be8b
Fredrick <otienofredricko409@gmail.com>
2023-07-25 05:37:00 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse followed by a new line
+ * @s: variable
+ * return: void
+ */
+void print_rev(char *s)
+{
+	int y = 0;
+	int z;
+
+	while (*s != '\0')
+	{
+		y++;
+		s++
+	}
+	s--;
+	for (y = z; l > 0; z--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
